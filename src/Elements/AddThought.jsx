1301dc989683214ac6b94c84ff95928524198871
johiny <johiny7@live.com>
2022-09-28 15:16:30 -0500

    testing mobile background behaviour 6

--- src/Elements/AddThought.jsx
+++ src/Elements/AddThought.jsx
@@ -61,7 +61,7 @@ const StyledAddThought = styled.div`
     top: -1vh;
     @media only screen and (max-width: 700px){
         position: fixed;
-        bottom: 2vh;
+        bottom: -90%;
         z-index: 30;
         left: -4vh;
     }
